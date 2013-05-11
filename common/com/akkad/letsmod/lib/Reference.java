09b7da1dc90b4b726a71922e65ec95f558ebc00b
Akk4d <donosor@hotmail.fr>
2013-05-11 23:11:14 +0200

    fsd

---
+++ common/com/akkad/letsmod/lib/Reference.java
@@ -0,0 +1,10 @@
+package com.akkad.letsmod.lib;
+
+public class Reference
+{
+
+    
+    public static final String MOD_ID = "LMM";
+    public static final String MOD_NAME = "LetsMod Mod";
+    public static final String VERSION = "0.0.1";
+}
